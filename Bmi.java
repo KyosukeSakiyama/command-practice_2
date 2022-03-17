303ccb2d66106763562019180eeccf91104b340e
Kyosuke Sakiyama <k.sakiyama.sys21@morijyobi.ac.jp>
2022-03-17 12:19:18 +0900

    [add]README.md

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Bmi {
 	public static void main( String[] args) {
 		Scanner sc = new Scanner(System.in);
         System.out.println("Height(cm)");
