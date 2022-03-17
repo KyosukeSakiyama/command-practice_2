8d927df7df5c5164b0ca4b039804c34321059671
Kyosuke Sakiyama <k.sakiyama.sys21@morijyobi.ac.jp>
2022-03-17 12:05:01 +0900

    [add] Main.java .gitignore
    git status

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+	public static void main( String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Height(cm)");
+        double h = sc.nextInt();
+            h = h / 100;
+        System.out.println("Weight(kg)");
+        double w = sc.nextInt();
+        double a;
+
+        a = w / ( h * h );
+        a = ((double)Math.round(a * 10))/10;
+
+        System.out.println( "Your BMI is:" + a + "!" );
+    }
+}
